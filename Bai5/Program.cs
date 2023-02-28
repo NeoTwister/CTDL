74c6e76928cc0e173c36f756dec857d9c3f092d6
Twisters <vuthanhdat26@gmail.com>
2023-02-28 18:57:32 +0700

    Init commit

---
+++ Bai5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bai5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Nhap so luong sao");
+            n = int.Parse(Console.ReadLine());
+            for(int i = 0; i<n; i++)
+            {
+                Console.Write("*");
+            }
+            Console.ReadKey();
+        }
+    }
+}
